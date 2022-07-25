7ee7edcdbb9a774d09e9182be7490f2632d4fd41
advokat13 <advocat328@gmail.com>
2022-07-25 08:44:06 +0300

    alc_new.py

--- calc_new.py
+++ calc_new.py
@@ -1,47 +1,45 @@
-while True:	
-	
-	def input_value():
-		x=()
+
+def input_value():
+		x = None
 		while type(x)!=int and type(x)!=float:
-			print('Введите значение')
-			x=input()	
+			x = input('Введите значение')	
 			if ',' in x:
-				x1=x.replace(',','.')
-				x=x1	
+				x1 = x.replace(',','.')
+				x = x1	
 			if '.' in x:
 				try:
-					x=float(x)
+					x = float(x)
 				except:
 					print('Вы ввели не число')
 			else:
 				try:
-					x=int(x)
+					x = int(x)
 				except:
 					print('Вы ввели не число')
 		return x
+				
+while True:	
+	a = input_value()
+	b = input_value()
 	
-	a=input_value()
-	b=input_value()
-	
-	print('Введите +,-,/ или *')
-	x=()
+	x = None
 	while x !='+' and x !="-" and x !='/' and x !='*':
-		x=input(x)
-		if x=='+':
+		x = input('Введите +,-,/ или *')
+		if x == '+':
 			print(a+b)
-		elif x=='-':
+		elif x == '-':
 			print(a-b)
-		elif x=='/':
+		elif x == '/':
 			try:
 				print(a/b)
 			except ZeroDivisionError:
 				print('Деление на ноль невозможно')
-		elif x=='*':
+		elif x == '*':
 			print(a*b)
 		else:
 			print('введите +,-,/,*')
 	
 	print('Для выхода из калькулятор нажмите "q". Для продолжения работы нажмите любой другой символ')
-	y=str(input())
-	if y=='q':
+	y = str(input())
+	if y == 'q':
 		break
